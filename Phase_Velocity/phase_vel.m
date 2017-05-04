d15b99e7b596ff9bf392e3ef4f371f3b55143b43
Hamzeh <hamzeh.sadeghi@gmail.com>
2017-05-04 13:10:42 +0200

    saving to file issue fixed

--- Phase_Velocity/phase_vel.m
+++ Phase_Velocity/phase_vel.m
@@ -5,7 +5,7 @@ function phase_vel()
 %
 % June 2016
 % changed: Aug 2016
-% last modification: 14 Feb 2017
+% last modification: 4 May 2017
 % Hamzeh Sadeghisorkhani
 
 %% Parameters
@@ -787,11 +787,13 @@ function donefnc(~, ~)
 
         if strcmp(params.outputformat,'txt')==1            
             txtresult= nan(max([length(Sdisp);length(Intdisp);length(prds)]),10);
-            for i=1:length(Sdisp)
+            [n3,~]= size(Sdisp);
+            for i=1:n3
                 txtresult(i,1)=  Sdisp(i,1);
                 txtresult(i,2)=  Sdisp(i,2);
             end
-            for i=1:length(Intdisp)
+            [n3,~]= size(Intdisp);
+            for i=1:n3
                 txtresult(i,3)=  Intdisp(i,1);
                 txtresult(i,4)=  Intdisp(i,2);
             end
@@ -1008,6 +1010,35 @@ function [ref_disp, zz, icons]= rddata(d, reference, format, delimval)
     end
 
 end
+
+
+
+
+%%
+% Copyright (c) 2016, Hamzeh Sadeghisorkhani
+% All rights reserved.
+%
+% 
+% 1) TERMS OF USE
+%   If you use GSpecDisp or any function(s) of it, you need to acknowledge 
+%   GSpecDisp by citing the following article:
+%   Sadeghisorkhani, H., Gudmundsson, O.. GSpecDisp: a Matlab GUI 
+%   package for phase-velocity dispersion measurements from ambient-noise 
+%   correlations, Computers and Geosciences, ??.   
+%   
+% 
+% 2) LICENSE:
+%   This program is part of GSpecDisp
+%   GSpecDisp is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%      GSpecDisp is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%       You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
